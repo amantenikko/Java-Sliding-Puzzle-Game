d556fccf492c9c434f1769a96c3dccdb70e4d737
Nikko Amante <84558605+amantenikko@users.noreply.github.com>
2022-09-06 03:57:02 +0800

    Add files via upload

---
+++ SlidingPuzzle.java
@@ -0,0 +1,141 @@
+import javax.swing.*;
+import javax.swing.Timer;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class SlidingPuzzle extends JFrame implements ActionListener
+{
+	int row = 3, 
+		col = 3, 
+		state = (row * col) - 1,
+		seed = 0,
+		hr,mm,ss;
+		
+	Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+	LinkedList<ImageIcon> shuffler = new LinkedList<ImageIcon>();  
+	Random rand = new Random();
+	JPanel panel = new JPanel();
+
+	ImageIcon img[] = new ImageIcon[row * col];
+	JButton cell[] = new JButton[row * col],
+			shflBtn = new JButton("New Game");;
+	JLabel time = new JLabel("Time: 00h:00m:00s"),
+			credits = new JLabel("</> with <3 by Nikko Amante");
+	String imageList[] = { 
+		".img/1.jpg", ".img/2.jpg", ".img/3.jpg",
+		".img/4.jpg", ".img/5.jpg", ".img/6.jpg",
+		".img/7.jpg", ".img/8.jpg", ""
+	};
+
+	Timer timer = new Timer(1000 , new ActionListener() {
+		public void actionPerformed(ActionEvent evt) {
+			ss++;
+			if (ss >= 60) {
+				mm++; ss = 0;
+			}
+			if (mm >= 60) {
+				hr++; mm = 0; ss = 0;
+			}
+			time.setText("Time: " + ((hr < 9) ? "0" : "") + hr + "h:" + 
+									((mm < 9) ? "0" : "") + mm + "m:" + 
+									((mm < 9) ? "0" : "") + ss + "s");
+		}
+	});
+
+	SlidingPuzzle() {
+		for(int i=0; i < (row * col); i++) {
+			img[i] = new ImageIcon(imageList[i]);
+			shuffler.add(img[i]);
+			cell[i] = new JButton(img[i]);
+			cell[i].setBackground(new Color(200,200,200));
+			cell[i].addActionListener(this);
+			panel.add(cell[i]);
+		}
+		cell[(row * col) - 1].setEnabled(false);
+		shuffler.remove(img[(row * col) - 1]);
+		
+		panel.setBounds(20, 40, 200 * col, 200 * row);
+		panel.setLayout(new GridLayout(row, col, 0, 0));
+		add(panel);
+
+		shflBtn.setBounds(20, 10, (200 * col) / 2, 20);
+		shflBtn.addActionListener(this);
+		shflBtn.setFocusPainted(false);
+		shflBtn.setContentAreaFilled(false);
+		add(shflBtn);
+
+		time.setBounds(((200 * col) / 2) + ((200 * col) / 2) / 2, 10, 200 * col, 20);
+		add(time);
+
+		credits.setBounds(((200 * col) / 2) - 65, (200 * row) + 45, 200 * col, 20);
+		add(credits);
+
+		shflBtn.doClick();
+		timer.setRepeats(true);
+
+		setLayout(null);
+		setTitle(this.getClass().getCanonicalName());
+		setSize((200 * col) + 55, (200 * row) + 115);
+		setVisible(true);
+		setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource()==shflBtn) {
+			timer.stop();
+			seed++;
+			Collections.shuffle(shuffler, new Random(seed));
+			for(int a = 0; a < (row * col)-1; a++) {
+				cell[a].setEnabled(true);
+				cell[a].setIcon(shuffler.get(a));
+			}
+			state = (row * col) - 1;
+			cell[state].setIcon(img[(row * col) - 1]);
+			cell[state].setEnabled(false);
+			
+			hr = 0;
+			mm = 0;
+			ss = 0;
+			time.setText("Time: 00h:00m:00s");
+		}
+		
+		for(int a = 0; a < (row * col); a++) {
+			if (e.getSource()==cell[a])
+			{
+				if (!timer.isRunning()) timer.start();
+				for(int b = 1; b <= row; b++) {
+					if ((state == ((row * b) - 1) && (a == (row * b))) ||
+					 (state == ((row * b)) && (a == ((row * b) - 1)))) return;
+				}
+				if ((state == a + 1 || state == a - 1 || state == a + row || state == a - row)) {
+					cell[state].setIcon(cell[a].getIcon());
+					cell[state].setEnabled(true);
+					state = a;
+					cell[a].setIcon(img[(row * col) - 1]);
+					cell[a].setEnabled(false);
+				}
+			}
+		}
+
+		int counter = 0;
+		for(int i=0; i < (row * col); i++) {
+			if (cell[i].getIcon() == img[i]) {
+				counter++;
+			} else {
+				break;
+			}
+		}
+		if (counter == row * col) {
+			timer.stop();
+			JOptionPane.showMessageDialog(this, "Congratulations you won!!");
+		}
+	}
+	
+	public static void main(String[] args) {
+		new SlidingPuzzle();
+	}
+}
