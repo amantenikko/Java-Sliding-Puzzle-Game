5d36f7c72d9450ed81bdd1c996e82696ca04c5e2
Nikko Amante <84558605+amantenikko@users.noreply.github.com>
2022-09-10 15:42:56 +0800

    add more features and several fixes.

--- SlidingPuzzle.java
+++ SlidingPuzzle.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import javax.swing.Timer;
+import javax.swing.border.LineBorder;
 
 import java.awt.*;
 import java.awt.event.*;
@@ -7,12 +8,14 @@ import java.util.*;
 
 public class SlidingPuzzle extends JFrame implements ActionListener
 {
-	int row = 3, 
-		col = 3, 
+	boolean gameover;
+	int row = 3,
+		col = 3,
+		cellsize = 100,
 		state = (row * col) - 1,
 		seed = 0,
 		hr,mm,ss;
-		
+
 	Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 	LinkedList<ImageIcon> shuffler = new LinkedList<ImageIcon>();  
 	Random rand = new Random();
@@ -23,11 +26,7 @@ public class SlidingPuzzle extends JFrame implements ActionListener
 			shflBtn = new JButton("New Game");;
 	JLabel time = new JLabel("Time: 00h:00m:00s"),
 			credits = new JLabel("</> with <3 by Nikko Amante");
-	String imageList[] = { 
-		"./img/1.jpg", "./img/2.jpg", "./img/3.jpg",
-		"./img/4.jpg", "./img/5.jpg", "./img/6.jpg",
-		"./img/7.jpg", "./img/8.jpg", ""
-	};
+	String imageList[] = new String[row * col];
 
 	Timer timer = new Timer(1000 , new ActionListener() {
 		public void actionPerformed(ActionEvent evt) {
@@ -46,38 +45,47 @@ public class SlidingPuzzle extends JFrame implements ActionListener
 
 	SlidingPuzzle() {
 		for(int i=0; i < (row * col); i++) {
-			img[i] = new ImageIcon(imageList[i]);
+			imageList[i] = "/img/" + ( i + 1) + ".jpg";
+			if (getClass().getResource(imageList[i]) != null) {
+				img[i] = new ImageIcon(getClass().getResource(imageList[i]));
+				Image resizer = img[i].getImage();
+				img[i] = new ImageIcon(resizer.getScaledInstance(cellsize, cellsize, java.awt.Image.SCALE_SMOOTH));
+			}
 			shuffler.add(img[i]);
 			cell[i] = new JButton(img[i]);
 			cell[i].setBackground(new Color(200,200,200));
+			cell[i].setBorderPainted(true);
 			cell[i].addActionListener(this);
 			panel.add(cell[i]);
+			System.out.println(imageList[i]);
 		}
 		cell[(row * col) - 1].setEnabled(false);
 		shuffler.remove(img[(row * col) - 1]);
 		
-		panel.setBounds(20, 40, 200 * col, 200 * row);
+		panel.setBounds(20, 40, cellsize * col, cellsize * row);
 		panel.setLayout(new GridLayout(row, col, 0, 0));
+		panel.setBorder(new LineBorder(new Color(150,150,150), 2));
 		add(panel);
 
-		shflBtn.setBounds(20, 10, (200 * col) / 2, 20);
+		shflBtn.setBounds(20, 10, (cellsize * col) / 2, 20);
 		shflBtn.addActionListener(this);
 		shflBtn.setFocusPainted(false);
 		shflBtn.setContentAreaFilled(false);
 		add(shflBtn);
 
-		time.setBounds(((200 * col) / 2) + ((200 * col) / 2) / 2, 10, 200 * col, 20);
+		time.setBounds(((cellsize * col) / 2) + ((cellsize * col) / 2) / 2, 10, cellsize * col, 20);
 		add(time);
 
-		credits.setBounds(((200 * col) / 2) - 65, (200 * row) + 45, 200 * col, 20);
+		credits.setBounds(((cellsize * col) / 2) - 65, (cellsize * row) + 45, cellsize * col, 20);
 		add(credits);
 
-		shflBtn.doClick();
+		// shflBtn.doClick();
+		gameover = false;
 		timer.setRepeats(true);
 
 		setLayout(null);
 		setTitle(this.getClass().getCanonicalName());
-		setSize((200 * col) + 55, (200 * row) + 115);
+		setSize((cellsize * col) + 55, (cellsize * row) + 115);
 		setVisible(true);
 		setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -92,11 +100,13 @@ public class SlidingPuzzle extends JFrame implements ActionListener
 			for(int a = 0; a < (row * col)-1; a++) {
 				cell[a].setEnabled(true);
 				cell[a].setIcon(shuffler.get(a));
+				cell[a].setBorderPainted(true);
 			}
 			state = (row * col) - 1;
 			cell[state].setIcon(img[(row * col) - 1]);
 			cell[state].setEnabled(false);
 			
+			gameover = false;
 			hr = 0;
 			mm = 0;
 			ss = 0;
@@ -106,6 +116,7 @@ public class SlidingPuzzle extends JFrame implements ActionListener
 		for(int a = 0; a < (row * col); a++) {
 			if (e.getSource()==cell[a])
 			{
+				if (gameover) return;
 				if (!timer.isRunning()) timer.start();
 				for(int b = 1; b <= row; b++) {
 					if ((state == ((row * b) - 1) && (a == (row * b))) ||
@@ -123,14 +134,16 @@ public class SlidingPuzzle extends JFrame implements ActionListener
 
 		int counter = 0;
 		for(int i=0; i < (row * col); i++) {
-			if (cell[i].getIcon() == img[i]) {
+			if (cell[i].getIcon() == img[i]) 
 				counter++;
-			} else {
+			else 
 				break;
-			}
 		}
 		if (counter == row * col) {
 			timer.stop();
+			for(int a = 0; a < (row * col)-1; a++)
+				cell[a].setBorderPainted(false);
+			gameover = true;
 			JOptionPane.showMessageDialog(this, "Congratulations you won!!");
 		}
 	}
